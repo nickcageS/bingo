98a449a461d6c769296f6ffdaa68e5842812c2a0
PostMacode <jackactavis@gmail.com>
2020-05-12 19:15:16 -0400

    Updates

---
+++ precache-manifest.ff565c2db988992cd499541e1ba7034d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bb0926c837da0dd3a779d6f2c922dd8",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "3c3a702b42f5f47f3e59",
+    "url": "/bingo/static/css/main.0b4d2789.chunk.css"
+  },
+  {
+    "revision": "815f9e4fbc8e3dd6c20e",
+    "url": "/bingo/static/js/2.6e14d907.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bingo/static/js/2.6e14d907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3a702b42f5f47f3e59",
+    "url": "/bingo/static/js/main.cb364238.chunk.js"
+  },
+  {
+    "revision": "c77dfdbe55a21accc6ad",
+    "url": "/bingo/static/js/runtime-main.c71c266f.js"
+  }
+]);
